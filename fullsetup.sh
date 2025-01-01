f8781b3ae99051fdbff671a6a169cd52c6604436
root <root@DESKTOP-M7P3MKB.>
2025-01-01 15:51:23 +0300

    Ses yeteneği

--- fullsetup.sh
+++ fullsetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y ffmpeg portaudio19-dev alsa-utils mpg123 vlc
+sudo apt-get install -y ffmpeg portaudio19-dev alsa-utils vlc
+sudo chmod u+s /usr/bin/vlc-wrapper
 sudo python3 -m venv venv
 source venv/bin/activate
 sudo ./venv/bin/pip install -r requirements.txt
