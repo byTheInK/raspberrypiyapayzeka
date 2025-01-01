1b2cbd9cbcfc54f164efdbeefc9b212bc443eda4
root <root@DESKTOP-M7P3MKB.>
2025-01-01 15:03:01 +0300

    Raspberry PI

---
+++ fullsetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y ffmpeg portaudio19-dev alsa-utils
+sudo python3 -m venv venv
+source venv/bin/activate
+sudo ./venv/bin/pip install -r requirements.txt
+deactivate
